3c785d8e42f55bad08fe0909c9fb56ebbff70da2
Timea Mali <derzsi_timea@yahoo.com>
2024-07-20 16:53:44 +0000

    ADD models

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,7 @@ from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+
+
+# Register your models here.
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,17 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+
+
+    # Create your comment here.
+
+
+    class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
